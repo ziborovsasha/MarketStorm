9fa17e0a2bac638c00125a7f180168eb9413a0dd
ziborovsasha <ziborov_sasha@ukr.net>
2017-03-30 01:37:20 +0300

    30.03.17

--- markup/js/custom.js
+++ markup/js/custom.js
@@ -1,10 +1,21 @@
-$(function(){
-
-	
-
+(function($) {
+$(function() {
+	$('.styler').styler();
 });
 
 
 
 
+(function($) {
+$(function() {
+		$('.styler').styler();
+});
 
+		$('.more').click(function(){
+			$(this).parents('.news').find('.news__excerpt').slideToggle()
+			$(this).html('Скрыть')
+			$('.news__img').slideToggle()
+			return false;
+		});
+
+})(jQuery);
